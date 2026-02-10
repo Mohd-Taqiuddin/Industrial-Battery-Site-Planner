7cdeb04e1cdcae9994a69ef4773992639d5ed77e
Mohd-Taqiuddin <taqiuddin12345@gmail.com>
2026-02-10 12:14:47 -0600

    changed name in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ interface NavbarProps {
 
 export const Navbar: React.FC<NavbarProps> = ({ theme, toggleTheme }) => (
   <nav className="navbar">
-    <div className="brand">TESLA <span>Energy Planner</span></div>
+    <div className="brand">TESLA <span>Industrial Energy Battery Site Planner</span></div>
     <button onClick={toggleTheme} className="theme-btn">
       {theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
     </button>
